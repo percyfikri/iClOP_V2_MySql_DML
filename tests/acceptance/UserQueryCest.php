04731507125f993de8b17d49e048f7de25956a35
percyfikri <mafikri27@gmail.com>
2025-06-02 09:57:36 +0700

    Codeception Update: update untuk file uji coba dari Query Input User

--- tests/acceptance/UserQueryCest.php
+++ tests/acceptance/UserQueryCest.php
@@ -1,68 +1,28 @@
 <?php
 class UserQueryCest
 {
-    public function testInsertAndSelect(\Tests\Support\AcceptanceTester $I)
-    {
 
-        // $I->haveInDatabase('mk', [
-        //     'kode_mk' => '02010',
-        //     'nama_mk' => 'Basis Data'
-        // ]);
-
-        // // Eksekusi query SELECT dari user
-        // $I->seeInDatabase('mk', [
-        //     'kode_mk' => '02010',
-        //     'nama_mk' => 'Basis Data'
-        // ]);
-
-        $I->comment('Insert data ke tabel mk');
-        $I->haveInDatabase('mk', [
-            'kode_mk' => '02010',
-            'nama_mk' => 'Basis Data'
-        ]);
-        $I->comment('Cek data di tabel mk');
-        $I->seeInDatabase('mk', [
-            'kode_mk' => '02010',
-            'nama_mk' => 'Basis Data'
-        ]);
-    }
-
-    // public function testUserQuery(\Tests\Support\AcceptanceTester $I)
-    // {
-    //     $queryId = getenv('QUERY_ID') ?: (isset($_ENV['query_id']) ? $_ENV['query_id'] : null);
-    //     if (!$queryId) {
-    //         $I->fail('Query ID tidak ditemukan');
-    //     }
-    //     // Ambil query dari database utama (gunakan koneksi Laravel/Db utama jika perlu)
-    //     $query = $I->grabFromDatabase('mysql_queries', 'query', ['id' => $queryId]);
-
-    //     // Jalankan query user di database iclop_v2_testing
-    //     try {
-    //         $I->executeQuery($query); // gunakan $I->executeQuery jika runSql tidak ada
-    //         // Lakukan pengujian, misal cek data di tabel mk
-    //         // $I->seeInDatabase('mk', ['kode_mk' => '02010']);
-    //     } catch (\Exception $e) {
-    //         $I->fail('Query gagal dijalankan: ' . $e->getMessage());
-    //     }
-    // }
     public function testUserQuery(\Tests\Support\AcceptanceTester $I, \Codeception\Module\Db $db)
     {
+        // Tentukan nama file query yang akan dijalankan
         $queryFile = codecept_root_dir() . 'tests/query_user.sql';
+        
+        // Cek apakah file query_user.sql ada
         if (!file_exists($queryFile)) {
+            // Jika tidak ada, maka failed test
             $I->fail('File query_user.sql tidak ditemukan: ' . $queryFile);
         }
+        
+        // Baca isi file query_user.sql
         $query = file_get_contents($queryFile);
+        
+        // Jika isi file query_user.sql kosong, maka failed test
         if (!$query) {
             $I->fail('Query user kosong');
         }
-        // $I->executeQuery($query);
-        // // Lakukan pengujian hasil query di database testing
-
-        // Jalankan query user
-        // $I->getModule('Db')->_getDbh()->exec($query);
+        
+        // Jalankan query yang ada di file query_user.sql
         $db->_getDbh()->exec($query);
-
-        // Lakukan pengujian hasil query di database testing
-        // $I->seeInDatabase('mk', ['kode_mk' => '02010']);
     }
 }
+
